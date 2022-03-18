1fc2e1cbc857e4fe59e4ffa5d4fd69e41dfe969c
apodacaj1995 <contactjackiecodes@gmail.com>
2022-03-17 18:52:03 -0600

    commented code

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// brings these IDs in from index.html
 const toggle = document.getElementById('toggle')
 const nav = document.getElementById('nav')
 
+// waits for 'click' and toggles(ID) the button (EL)
 toggle.addEventListener('click', () =>{
     nav.classList.toggle('active')
 })
